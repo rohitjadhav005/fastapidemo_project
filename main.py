23eccc8a646b5e35ad316a865edbd71c90d22a89
rohitjadhav005 <rj8169552@gmail.com>
2026-03-24 19:16:41 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Path, HTTPException
+import json
+app = FastAPI()
+def load_data():
+    with open('pationt.json','r' ) as f:
+        data = json.load(f)
+    return data
+@app.get("/")
+def hello():
+    return{'message':"Patient Management System API"}
+@app.get("/about")
+def about():
+    return{'message':"A fully functional API to manage your patient records"}
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id:str = Path(...,description = 'ID of the patient in the DB',  example='P001')):
+    data = load_data()
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404,detail ='patient not found')
